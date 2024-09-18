6c77d1567be8c8f2996e0f78f123c972e8ecb500
dawidzurawski <dawidzurawskidrz@gmail.com>
2024-09-18 19:24:57 +0100

    Add carousel

--- src/components/Highlights.jsx
+++ src/components/Highlights.jsx
@@ -2,6 +2,8 @@ import gsap from 'gsap';
 import { useGSAP } from '@gsap/react';
 import { rightImg, watchImg } from '../utils';
 
+import VideoCarousel from './VideoCarousel';
+
 const Highlights = () => {
   useGSAP(() => {
     gsap.to('#title', { opacity: 1, y: 0 })
@@ -12,7 +14,6 @@ const Highlights = () => {
     <section id="highlights" className="w-screen overflow-hidden h-full common-padding bg-zinc">
       <div className="screen-max-width">
         <div className="mb-12 w-full md:flex items-end justify-between">
-
           <h1 id="title" className="section-heading">
             Get the highlights.
           </h1>
@@ -27,8 +28,9 @@ const Highlights = () => {
               <img src={rightImg} alt="right" className="ml-2"/>
             </p>
           </div>
-
         </div>
+
+          <VideoCarousel />
       </div>
     </section>
   )
