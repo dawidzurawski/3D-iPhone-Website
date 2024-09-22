ed55c1897846c600d388af6a5a4320f21b1cebb6
dawidzurawski <dawidzurawskidrz@gmail.com>
2024-09-22 20:41:23 +0100

    Add sentry

--- src/components/Model.jsx
+++ src/components/Model.jsx
@@ -7,6 +7,8 @@ import { yellowImg } from '../utils';
 import { Canvas } from '@react-three/fiber';
 import { View } from '@react-three/drei';
 import { models, sizes } from '../constants';
+import { useEffect } from 'react';
+import { animateWithGsapTimeline } from '../utils/animations';
 
 const Model = () => {
   const [size, setSize] = useState('small')
@@ -24,6 +26,32 @@ const Model = () => {
 
   const [smallRotation, setSmallRotation] = useState(0)
   const [largeRotation, setLargeRotation] = useState(0)
+
+  const tl = gsap.timeline();
+
+  useEffect(() => {
+    if(size === 'large') {
+      animateWithGsapTimeline(
+        tl,
+        small,
+        smallRotation,
+        '#view1',
+        '#view2',
+        { transform: 'translateX(-100%)', duration: 2 }
+      )
+    }
+
+    if(size === 'small') {
+      animateWithGsapTimeline(
+        tl,
+        large,
+        largeRotation,
+        '#view2',
+        '#view1',
+        { transform: 'translateX(0)', duration: 2 }
+      )
+    }
+  }, [size])
 
   useGSAP(() => {
     gsap.to('#heading', {
--- src/components/ModelView.jsx
+++ src/components/ModelView.jsx
@@ -1,15 +1,16 @@
 import { OrbitControls, PerspectiveCamera, View } from "@react-three/drei"
 import { Suspense } from "react"
 import Lights from "./Lights"
+import Loader from "./Loader"
 import IPhone from "./IPhone"
 import * as THREE from 'three'
 
-const ModelView = ({ index, groupRef, gsapType, controlRef, setRotationSize, size, item}) => {
+const ModelView = ({ index, groupRef, gsapType, controlRef, setRotationState, size, item}) => {
   return (
     <View
       index={index}
       id={gsapType}
-      className={`w-full h-full ${index === 2} ? right-[-100%] : '`}
+      className={`w-full h-full absolute ${index === 2 ? 'right-[-100%]' : ''}`}
     >
       <ambientLight intensity={0.3} />
 
@@ -32,7 +33,7 @@ const ModelView = ({ index, groupRef, gsapType, controlRef, setRotationSize, siz
         name={`${index === 1} ? 'small' : 'large'`}
         position={[0, 0, 0]}
       >
-        <Suspense>
+        <Suspense fallback={<Loader />}>
           <IPhone
             scale={index === 1 ? [15, 15, 15] : [17, 17, 17]}
             item={item}
